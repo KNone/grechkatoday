335c6ccc8719adc194a67cc52b33bafd7c0df226
Nikita Krasnoyartsev <i@knone.ru>
2014-12-14 21:27:17 +0300

    немного анимации

--- src/KNone/Grecha/Resources/views/index.php
+++ src/KNone/Grecha/Resources/views/index.php
@@ -5,32 +5,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <title>Гречка Тудэй | Сколько стоит гречка сегодня? #grechkatoday #гречкатудэй</title>
     <link rel="stylesheet" href="css/styles.css" type="text/css"/>
-    <meta name="keywords" content="гречка, греча, курс гречи, килограмм гречки, гречневая крупа, стоимость гречки, grechkatoday, grechka, гручка тудэй, гречка сегодня" />
-    <meta name="description" content="Сколько стоит гречка сегодня на прилавках российских магазинов" />
-    <link rel="icon" type="image/png" href="favicon.png" />
+    <meta name="keywords"
+          content="гречка, греча, курс гречи, килограмм гречки, гречневая крупа, стоимость гречки, grechkatoday, grechka, гручка тудэй, гречка сегодня"/>
+    <meta name="description" content="Сколько стоит гречка сегодня на прилавках российских магазинов"/>
+    <link rel="icon" type="image/png" href="favicon.png"/>
 </head>
 <body>
 <div class="k-layout">
     <div class="k-currency">
-        <p class="k-currency-amount"><?=$price->getValue()?> руб.</p>
-        <p class="k-currency-any"><?=$exchanger->exchangeToUsd($price->getValue())?>$ <?=$exchanger->exchangeToEur($price->getValue())?>€</p>
-        <p class="k-currency-description"><?=$price->getDateTime()->format('d.m.Y')?></p>
-        <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="small" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki" data-yashareTheme="counter"></div>
+        <p class="k-currency-amount"><?= $price->getValue() ?> руб.</p>
+
+        <p class="k-currency-description"><?= $price->getDateTime()->format('d.m.Y') ?></p>
+
+        <p class="k-currency-any"><?= $exchanger->exchangeToUsd($price->getValue()) ?>
+            $ <?= $exchanger->exchangeToEur($price->getValue()) ?>€</p>
+
+        <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="small"
+             data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki" data-yashareTheme="counter"></div>
     </div>
 </div>
 <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-57431312-1', 'auto');
     ga('require', 'displayfeatures');
     ga('send', 'pageview');
 </script>
+
 <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
 <script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script>
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,15 +1,30 @@
-$(document).ready(function () {
-    $(window).on('resize', function () {
-        setHeight();
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+        var $window = $(window);
+        $window.on('resize', function () {
+            setHeight($(this));
+        });
+        setHeight($window);
+        setPosition($('.k-currency'), $window);
     });
-    setHeight();
-});
-var setHeight = function () {
-    var $layout = $('.k-layout');
-    var $currency = $('.k-currency');
-    var windowHeight = $(window).height();
-    $layout.height(windowHeight);
-    $currency.css('margin-top', ((windowHeight - $currency.height()) / 2) + 'px');
-}
+    var setHeight = function ($window) {
+        var $layout = $('.k-layout');
+        var windowHeight = $window.height();
+        $layout.height(windowHeight);
+    };
+    var setPosition = function ($block, $window) {
+        var windowHeight = $window.height();
+        var top = windowHeight / 2 - $block.height() / 2;
+        $block.animate(
+            {
+                opacity: 1,
+                marginTop: top
+            },
+            1000
+        );
+    };
+})(jQuery);
+
 
 
