3301cf48fa0cb2237ba64897cb128d75cec29201
Nikita Krasnoyartsev <i@knone.ru>
2014-12-14 21:38:07 +0300

    fix анимация

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -2,18 +2,21 @@
     "use strict";
     $(document).ready(function () {
         var $window = $(window);
+        var $currency = $('.k-currency');
         $window.on('resize', function () {
-            setHeight($(this));
+            var __this = $(this);
+            setHeight(__this);
+            setPosition($currency, __this, 200);
         });
         setHeight($window);
-        setPosition($('.k-currency'), $window);
+        setPosition($currency, $window, 1000);
     });
     var setHeight = function ($window) {
         var $layout = $('.k-layout');
         var windowHeight = $window.height();
         $layout.height(windowHeight);
     };
-    var setPosition = function ($block, $window) {
+    var setPosition = function ($block, $window, timeout) {
         var windowHeight = $window.height();
         var top = windowHeight / 2 - $block.height() / 2;
         $block.animate(
@@ -21,7 +24,7 @@
                 opacity: 1,
                 marginTop: top
             },
-            1000
+            timeout
         );
     };
 })(jQuery);
