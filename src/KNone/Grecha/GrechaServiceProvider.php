b5508a5fab29eae45fe38a03b8385153551da564
Nikita Krasnoyartsev <i@knone.ru>
2014-12-18 19:33:58 +0300

    в конструктор лучше не передавать не структурированные данные, потому что хз, что там в массиве

--- src/KNone/Grecha/GrechaServiceProvider.php
+++ src/KNone/Grecha/GrechaServiceProvider.php
@@ -6,7 +6,6 @@ use Igorw\Silex\ConfigServiceProvider;
 use KNone\Grecha\Entity\Persistence\DbalExchangeRateRepository;
 use KNone\Grecha\Entity\Persistence\DbalPriceRepository;
 use KNone\Grecha\ExchangeRate\ExchangeRateConverterFactory;
-use KNone\Grecha\ExchangeRate\ExchangerFactory;
 use KNone\Grecha\ExchangeRate\Importer;
 use KNone\Grecha\ExchangeRate\XmlRateParser;
 use KNone\Grecha\View\TemplateEngine;
@@ -25,6 +24,9 @@ class GrechaServiceProvider implements ServiceProviderInterface
      */
     private $configFilename;
 
+    /**
+     * @param string $configFilename
+     */
     public function __construct($configFilename)
     {
         $this->configFilename = $configFilename;
@@ -59,7 +61,13 @@ class GrechaServiceProvider implements ServiceProviderInterface
         });
 
         $app['grecha.price.price_strategy'] = function () use ($app) {
-            return new PriceStrategy($app['config']['price']['strategy']);
+            $strategyParams = $app['config']['price']['strategy'];
+
+            return new PriceStrategy(
+                $strategyParams['average'],
+                $strategyParams['deviation'],
+                $strategyParams['retail_rate']
+            );
         };
 
         $app['grecha.price.importer'] = function () use ($app) {
