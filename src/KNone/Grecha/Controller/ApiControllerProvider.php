6e05da978c9c7d3738c74100dc2540315c9fae57
Nikita Krasnoyartsev <i@knone.ru>
2014-12-18 20:49:27 +0300

    получение статистики ajax запросом

--- src/KNone/Grecha/Controller/ApiControllerProvider.php
+++ src/KNone/Grecha/Controller/ApiControllerProvider.php
@@ -2,9 +2,12 @@
 
 namespace KNone\Grecha\Controller;
 
+use KNone\Grecha\Entity\PriceRepositoryInterface;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ApiControllerProvider implements ControllerProviderInterface
 {
@@ -13,8 +16,30 @@ class ApiControllerProvider implements ControllerProviderInterface
         /** @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
 
-        $controllers->get('/prices/week', function (Application $app) {
-            return 'test';
+        $controllers->get('/prices/{interval}', function ($interval, Request $request, Application $app) {
+            if (!$request->isXmlHttpRequest()) {
+                throw new NotFoundHttpException();
+            }
+            /** @var PriceRepositoryInterface $priceRepository */
+            $priceRepository = $app['grecha.price.repository'];
+            if ($interval === 'week') {
+                $prices = $priceRepository->findPricesForWeek();
+            } else if ($interval === 'month') {
+                $prices = $priceRepository->findPricesForMonth();
+            } else {
+                throw new NotFoundHttpException();
+            }
+
+            $response = [];
+
+            foreach ($prices as $price) {
+                $response[] = [
+                    'value' => $price->getValue(),
+                    'date' => $price->getDateTime()->format('d.m.Y')
+                ];
+            }
+
+            return $app->json($response);
         });
 
         return $controllers;
