78501f4b4a6b9c74a1fd5092e5f519438e0389e8
Filimoshin Oleg <olegator8800@yandex.ru>
2014-12-29 22:51:33 +0500

    Прикрутил Symfony Filesystem

--- src/KNone/Grecha/Command/CacheClear.php
+++ src/KNone/Grecha/Command/CacheClear.php
@@ -7,7 +7,7 @@ use Knp\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use RuntimeException;
+use Symfony\Component\Filesystem\Filesystem;
 
 class CacheClear extends Command
 {
@@ -19,43 +19,25 @@ class CacheClear extends Command
     }
 
     /**
-     * @param  string $dirPath
-     * @return bool
-     */
-    protected function deleteDir($dirPath) {
-        if (!is_dir($dirPath)) {
-            return false;
-        }
-        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
-            $dirPath .= '/';
-        }
-
-        $files = glob($dirPath . '*', GLOB_MARK);
-
-        foreach ($files as $file) {
-            if (is_dir($file)) {
-                $this->deleteDir($file);
-            } else {
-                unlink($file);
-            }
-        }
-
-        return rmdir($dirPath);
-    }
-
-    /**
      * @param InputInterface $input
      * @param OutputInterface $output
      * @return int|null|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {     
+        $fs = new Filesystem();
         $rootDir = $this->getSilexApplication()['root_dir'].'/';
         $cacheDirs = $this->getSilexApplication()['config']['cache']['dir'];
 
         foreach ($cacheDirs as $cacheName => $cacheDir) {
             $cacheDir = $rootDir.$cacheDir;
-            $output->writeln(sprintf('Clear %s cache (%s)', $cacheName, $cacheDir));
+
+            if ($fs->exists($cacheDir)) {
+                $fs->remove($cacheDir);
+                $output->writeln(sprintf('Clear %s cache (%s)', $cacheName, $cacheDir));
+            } else {
+                $output->writeln(sprintf('Skip %s cache (%s)', $cacheName, $cacheDir));
+            }
         }
 
         $output->writeln('Chache clear finished');
