81e972143ff4ec7309ac2042e0c209caa4d89354
pus <i@knone.net>
2015-02-01 23:27:20 +0300

    небольшой рефакторинг

--- src/KNone/Grecha/Preview/PreviewCreator.php
+++ src/KNone/Grecha/Preview/PreviewCreator.php
@@ -6,17 +6,10 @@ use Symfony\Component\Process\ProcessBuilder;
 
 class PreviewCreator implements PreviewCreatorInterface
 {
-
     /**
      * @param string $filePath
      */
     public function createPreview($filePath)
-    {
-        $this->snapshot($filePath);
-
-    }
-
-    private function snapshot($filePath)
     {
         $processBuilder = new ProcessBuilder();
         $processBuilder->setPrefix('cutycapt');
@@ -26,12 +19,8 @@ class PreviewCreator implements PreviewCreatorInterface
             '--url=http://grechkatoday.ru',
             '--out=' . $filePath
         ];
-        var_dump($filePath);
         $processBuilder->setArguments($arguments);
         $processBuilder->getProcess()->run();
-
-//        cutycapt --url=http://grechkatoday.ru --out=t.png
     }
-
 }
 
