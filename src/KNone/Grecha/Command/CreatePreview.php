6d25f191b311daf070447848da8b07355ebb6459
pus <i@knone.net>
2015-02-01 23:20:57 +0300

    запилил побыстрому превьюшки для поделиться в соц сетях. Надо будет порефакторить кой чего

---
+++ src/KNone/Grecha/Command/CreatePreview.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace KNone\Grecha\Command;
+
+use KNone\Grecha\Preview\PreviewCreator;
+use Knp\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreatePreview extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:preview:create')
+            ->setDescription('');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $previewCreator = new PreviewCreator();
+        $previewCreator->createPreview($this->getSilexApplication()['root_dir'] . '/web/images/share/snapshot.png');
+
+        return 0;
+    }
+}
