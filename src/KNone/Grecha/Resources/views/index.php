5926a6e1c1e5a6b30d30e943b662abefd5ffb7f7
Nikita Krasnoyartsev <i@knone.ru>
2015-03-25 12:42:10 +0000

    откатил обратно

--- src/KNone/Grecha/Resources/views/index.php
+++ src/KNone/Grecha/Resources/views/index.php
@@ -129,17 +129,22 @@
 </div>
 
 <script <?= ($debug ? 'data-main="js/app"' : 'data-main="js/scripts"') ?> src="js/lib/require.js"></script>
-
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-  ga('create', 'UA-61189058-1', 'auto');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
-
+    ga('create', 'UA-57431312-1', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
 </script>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </body>
